fa35231d87339a4ce5914b870fdf94cd02df8bf1
Vito <vito95v@gmail.com>
2021-08-31 15:59:24 +0200

    Push Snack 1

---
+++ Snack 1/js/main.js
@@ -0,0 +1,41 @@
+// Snack 1
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+// BONUS: inserire una arrow function che preso in input l'array di bici ritorni l'oggetto con bici più leggera
+
+const bici = [
+   {
+      nome: "Mountain-Bike_1",
+      peso: 5,
+   },
+   {
+      nome: "Mountain-Bike_2",
+      peso: 3,
+   },
+   {
+      nome: "Mountain-Bike_3",
+      peso: 8,
+   },
+];
+
+// const element = bici[i];
+// const { peso } = bici;
+// console.log(peso);
+
+let pesoMinore = Number.POSITIVE_INFINITY;
+let nomeMinore = "";
+
+for (let i = 0; i < bici.length; i++) {
+   const variabile = bici[i];
+   const { peso } = variabile;
+   console.log(peso);
+
+   let tmp = bici[i].peso;
+   if (tmp < pesoMinore) {
+      pesoMinore = tmp;
+      nomeMinore = bici[i].nome;
+   }
+}
+
+console.log(`La bici piu' leggera e' ${nomeMinore} con peso ${pesoMinore}`);
